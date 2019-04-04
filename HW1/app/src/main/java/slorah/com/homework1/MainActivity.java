724ca507e8bab34a707edaadffdb421327fe1558
Anu Slorah <anuslorah@hotmail.com>
2019-04-03 21:30:56 -0700

    first homework

---
+++ HW1/app/src/main/java/slorah/com/homework1/MainActivity.java
@@ -0,0 +1,17 @@
+package slorah.com.homework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView myText = new TextView(this);
+        myText.setText("My name is Anu Slorah. \nI am front end developer \nright now I am most excited about Python.");
+        setContentView(myText);
+    }
+}
