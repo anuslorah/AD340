3b046239daa42a4d3178c8c132bb4f0d79d6ab1a
Anu Slorah <anuslorah@hotmail.com>
2019-04-25 14:38:53 -0700

    Update DetailActivity.java

--- HW3/app/src/main/java/slorah/com/homework3/DetailActivity.java
+++ HW3/app/src/main/java/slorah/com/homework3/DetailActivity.java
@@ -3,8 +3,9 @@ package slorah.com.homework3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends Activity {
 
@@ -28,18 +29,21 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.recycler_card_detail);
 
         TextView title1 = findViewById(R.id.title);
-        title1.setText( movie[position].getTitle()));
+        title1.setText(movie[position].getTitle());
+        //Log.i(TAG, movie[position].getTitle());
 
-        TextView year = findViewById(R.id.year);
-        year.setText(Integer.toString(movie[position].getYear()));
+        TextView year1 = findViewById(R.id.year);
+        year1.setText(Integer.toString(movie[position].getYear()));
+
+        // from http://square.github.io/picasso/
+        ImageView pic = (ImageView)findViewById(R.id.pic);
+        Picasso.get().load(movie[position].getImageLink()).into(pic);
 
         TextView director = (TextView)findViewById(R.id.director);
         director.setText(movie[position].getDirector());
 
         TextView description = (TextView)findViewById(R.id.description);
         description.setText(movie[position].getDescription());
-
-
     }
 }
 
